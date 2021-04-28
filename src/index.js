53d3f4418d9ad116838fb1fa3b61713c72144dab
kkretschmerhensley <k.kretschmerhensley@gmail.com>
2021-04-27 20:03:02 -0500

    was editing the wrong file
    hopefully this works now...

--- src/index.js
+++ src/index.js
@@ -1,36 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-import { withAuthenticator } from '@aws-amplify/ui-react';
-
-import logo from './logo.svg';
-import './App.css';
-
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-      </header>
-    </div>
-  );
-}
-
-export default withAuthenticator(App, { includeGreetings: true });
-
-
-// Amplify.configure(config);
-//
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
