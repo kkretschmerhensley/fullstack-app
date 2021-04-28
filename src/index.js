df2ed971f54a0748a4040ecd4c7c44cf45800460
kkretschmerhensley <k.kretschmerhensley@gmail.com>
2021-04-27 19:45:34 -0500

    added imports and replaced placeholder code
    with code from book

--- src/index.js
+++ src/index.js
@@ -4,14 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App, { includeGreetings: true });
+
+
+// Amplify.configure(config);
+//
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
